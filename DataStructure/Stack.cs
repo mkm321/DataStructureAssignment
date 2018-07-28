213f4ff6edb152087b7cfac92a0b1a7794af3b60
Mohit Mehndiratta <mkm321.mm@gmail.com>
2018-07-28 08:36:04 +0530

    Update Stack.cs

--- DataStructure/Stack.cs
+++ DataStructure/Stack.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,41 +8,41 @@ namespace DataStructure
 {
     class Stack : ICommonOperations
     {
-        int[] stackArray=new int[10000];
-        int top = -1;
+        private int[] m_stackArray = new int[10000];
+        private int m_top = -1;
         public int GetTopValue()
         {
             Console.WriteLine();
-            return stackArray[top];
+            return m_stackArray[m_top];
         }
         public void Add(int data)
         {
-            stackArray[++top] = data;
+            m_stackArray[++m_top] = data;
         }
         public void Remove()
         {
-            top--;
+            m_top--;
         }
         public void Display()
         {
-            int current = top;
-            while (current != -1)
+            int currentPosition = m_top;
+            while (currentPosition != -1)
             {
-                Console.Write(stackArray[current] + " ");
-                current--;
+                Console.Write(m_stackArray[currentPosition] + " ");
+                currentPosition--;
             }
         }
         public void Sort()
         {
-            for(int i = 0; i <= top; i++)
+            for(int outerIndex = 0; outerIndex <= m_top; outerIndex++)
             {
-                for(int j = i; j <= top; j++)
+                for(int innerIndex = outerIndex; innerIndex <= m_top; innerIndex++)
                 {
-                    if (stackArray[j] > stackArray[i])
+                    if (m_stackArray[innerIndex] > m_stackArray[outerIndex])
                     {
-                        int temp = stackArray[i];
-                        stackArray[i] = stackArray[j];
-                        stackArray[j] = temp;
+                        int temp = m_stackArray[outerIndex];
+                        m_stackArray[outerIndex] = m_stackArray[innerIndex];
+                        m_stackArray[innerIndex] = temp;
                     }
                 }
             }
